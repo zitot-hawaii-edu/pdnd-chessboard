3746ae85cba61b033dac45056fba820e42ee9277
Oracle Public Cloud User <opc@instance-for-oraclecloudtutorial.sub12120003290.vcnfororacleclo.oraclevcn.com>
2024-12-17 19:18:52 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chessboard from './Chessboard';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Chessboard />
+    </div>
+  );
+}
+
+export default App;
+
